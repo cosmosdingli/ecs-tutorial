d9eb009fb9cb141350278bbffba5e894db312b93
natsuki <natsuki@sample.com>
2022-06-09 13:07:19 +0900

    no message

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
+  const msg = 'Hello from /test Node!a01';
   res.send(getPage(msg));
 });
 
